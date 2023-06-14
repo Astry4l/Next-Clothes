8eea91d9a69cafc36482e6636812cf65519f242d
Luciebdr <boudoire.lucie@gmail.com>
2023-06-14 16:09:14 +0200

    bouton filtre
    J'ai alligné les boutons filtres

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ function clearText() {
     button.innerHTML = "";
   }
 
-  
+  const filtres = document.querySelectorAll('.filtre');
+    filtres.forEach(filtre => {
+      filtre.addEventListener('click', () => {
+        if (!filtre.classList.contains('active')) {
+          // Supprime la classe "active" de tous les filtres
+          filtres.forEach(f => f.classList.remove('active'));
+          // Ajoute la classe "active" au filtre cliqué
+          filtre.classList.add('active');
+        }
+      });
+    }); 
